5d460bfe86a837c8785ff963c63d5114c0bfcb56
Murat Fatih ARKAN <muratfatiharkan@yandex.com.tr>
2019-12-03 11:17:30 +0300

    adding docker support with both api and web

--- FoodStore/Program.cs
+++ FoodStore/Program.cs
@@ -22,6 +22,7 @@ namespace FoodStore
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:5000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
